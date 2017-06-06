30df515d09c8d28128c741d61ccee1a0ae60c58a
tkoar <terrancekoar@gmail.com>
2017-06-06 15:30:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function forEach(iterable, callback) {
   if (Array.isArray(iterable)) {
-    for (let i = 0, l = iterable.length; i < l; i++) {
-      const element = iterable[i];
+    for (let i = 0; i < iterable.length; i++) {
+      var element = iterable[i];
       callback(element, i, iterable);
     }
   } else if (typeof iterable === 'object') {
-    for (const key in iterable) {
-      const value = iterable[key];
+    for (var key in iterable) {
+      var value = iterable[key];
       callback(value, key, iterable);
     }
   }
